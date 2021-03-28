b2e356bfb49725f612ff591f97d50c56885809b6
Witch <vorelovingwitch@gmail.com>
2021-03-28 21:41:51 +0300

    feat: complete transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,63 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/*arr*/) {
-  throw new CustomError('Not implemented');
+module.exports = function transform(arr) {
+  res = []
 
-  // let stop = false;
-  
-  // if(Array.isArray(arr)){
-  //   if(!(arr.includes("--double-next") || arr.includes("--double-prev") || arr.includes("--discard-next") || arr.includes("--discard-prev"))){
-  //     return arr
-  //   }
-  //   else{
-  //     for(let i = 0; i < arr.length; i++) {
-  //       console.log(stop, arr)
-  //       if(typeof arr[i] == "string" || typeof arr[i] == "number" || typeof arr[i] == "bool") {
-  //         if(arr[i]=='--double-prev'){
-  //           if(i != 0  && !stop){
-  //             arr[i] = arr[i-1]
-  //           }
-  //           else{
-  //             arr.splice(i,1)
-  //           }
-  //         }
-  //         if(arr[i]=='--double-next'){
-  //           if(i!=arr.length-1  && !stop){
-  //             arr[i] = arr[i+1]
-  //           }
-  //           else{
-  //             arr.splice(i,1)
-  //           }
-  //         }
-  //         if(arr[i]=='--discard-prev'){
-  //           if(i > 0 && !stop){
-  //             arr.splice(i-1, 1)
-  //             arr.splice(i-1, 1)
-  //           }
-  //           else{
-  //             arr.splice(i, 1)
-  //           }
-  //           stop = true
-            
-  //           i=i-1
-  //         }
-  //         if(arr[i]=='--discard-next'){
-  //           if(i<arr.length-1 && !stop){
-  //             arr.splice(i+1, 1)
-              
-  //           }
-  //           stop = true
-  //           arr.splice(i, 1)
-  //           i=i-1
-  //         }
-  //       }
-  //       else{
-  //         return arr
-  //       }
-  //     }
-  //     return arr
-  //   }
-  // }
-  // else
-  //   return 'THROWN'
+  if(!Array.isArray(arr))
+    throw new Error("")
+
+  if(arr.length == 0){
+    return res
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--discard-next":
+        if (i < arr.length) {
+          i++;
+          if (arr[i + 1] == "--discard-prev" || arr[i + 1] == "--double-prev") {
+            i++;
+          }
+        }
+        break;
+      case "--discard-prev":
+        if (i != 0) {
+          res.pop();
+        }
+        break;
+      case "--double-next":
+        if (i < arr.length - 1) {
+          res.push(arr[i + 1]);
+        }
+        break;
+      case "--double-prev":
+        if (i != 0) {
+          res.push(arr[i - 1]);
+        }
+        break;
+      default:
+        res.push(arr[i]);
+        break;
+    }
+  }
+
+  return res;
 };
