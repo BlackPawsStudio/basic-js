f14c3fdc62849131ba53af069af71f2a2eaf935b
Witch <vorelovingwitch@gmail.com>
2021-03-28 23:16:26 +0300

    feat: complete extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
+
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (options.separator==undefined)
+    options.separator="+";
+  if (options.additionSeparator==undefined)
+    options.additionSeparator="|";
+  function builder(str, num, r){
+    let temp=[];
+    if (str==="undefined"&&str!=="null") 
+      return "";
+    if (num==undefined) 
+      return str;
+    for (let i=0; i< num; i++){
+      temp.push(str);
+    }
+    return temp.join(r);
+  }
+  let result=builder(str+"",options.repeatTimes,`${builder(options.addition+"",options.additionRepeatTimes,options.additionSeparator)}${options.separator}`)+builder(options.addition+"",options.additionRepeatTimes,options.additionSeparator);
+  return result;
 };
-  
